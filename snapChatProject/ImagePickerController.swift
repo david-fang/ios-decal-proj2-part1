520a3c5ceb74f22b47b4af25800851ca59c63aa8
David Fang <i.davidfang@gmail.com>
2017-03-10 12:43:06 -0800

    Work on feed posting logic

--- snapChatProject/ImagePickerController.swift
+++ snapChatProject/ImagePickerController.swift
@@ -15,6 +15,10 @@ class ImagePickerController: UIViewController, UICollectionViewDataSource, UICol
         super.viewDidLoad()
         self.imageCollectionView.collectionViewLayout = ImageFlowLayout.init()
         self.imageCollectionView.backgroundColor = UIColor.lightGray
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title:"", style:.plain, target:nil, action:nil)
+        
+        
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,24 +28,35 @@ class ImagePickerController: UIViewController, UICollectionViewDataSource, UICol
 
     func selectImage(_ image: UIImage) {
         //The image being selected is passed in as "image".
+        performSegue(withIdentifier: "ShowPostFeeds", sender: self)
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "ShowPostFeeds") {
+            if let dest = segue.destination as? PostFeedsTableViewController {
+                // FIX ME
+            }
+        }
     }
     
     
     
     //DON'T MODIFY CODE HERE AND BELOW!
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return allImages.count
     }
-    
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "pickImageCell", for: indexPath) as! imageCollectionVieCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "pickImageCell", for: indexPath) as! ImageCollectionViewCell
         cell.image.image = allImages[indexPath.row]
         return cell
     }
+
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let selectedCell = collectionView.cellForItem(at: indexPath) as! imageCollectionVieCell
+        let selectedCell = collectionView.cellForItem(at: indexPath) as! ImageCollectionViewCell
         selectImage(selectedCell.image.image!)
     }
 }
---
+++ snapChatProject/ThreadViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ThreadViewCell.swift
+//  snapChatProject
+//
+//  Created by David Fang on 3/10/17.
+//  Copyright © 2017 org.iosdecal. All rights reserved.
+//
+
+import UIKit
+
+class ThreadViewCell: UITableViewCell {
+
+    @IBOutlet weak var threadName: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
